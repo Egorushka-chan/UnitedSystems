390091c92ba4da8abe748bc436a544571413338d
Egor <umenya2003@mail.ru>
2024-07-22 12:57:21 +0400

    Исправление названия WardrobeOnline

---
+++ MasterDominaSystem/MasterDominaSystem.RMQL/Implementations/RabbitConsumer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MasterDominaSystem.RMQL.Abstractions;
+
+namespace MasterDominaSystem.RMQL.Implementations
+{
+    public class RabbitConsumer : IBrokerConsumer
+    {
+        public Task<string> GetMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
